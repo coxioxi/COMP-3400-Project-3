acbc181322269b3c4f16b786a5e4f20d1e42ea3b
antran252 <87943434+antran252@users.noreply.github.com>
2025-04-10 12:01:54 -0400

    Update cgi_response.c

--- project3/cgi_response.c
+++ project3/cgi_response.c
@@ -27,7 +27,7 @@ char *
 cgi_response (char *uri, char *version, char *method, char *query,
               ssize_t size, char *boundary, char *body)
 {
-	
+
   // TODO [PART]: If the URI exists and is executable, run it as a separate
   // process, redirecting its STDOUT back to this process. You can then use
   // that resulting string to determine the Content-Length to send back. As
@@ -46,164 +46,176 @@ cgi_response (char *uri, char *version, char *method, char *query,
   //   "<h2>Hello world!</h2>\n"
   //   "</body>\n"
   //   "</html>\n"
-  
-  char* response = NULL;
+
+  char *response = NULL;
   if (uri != NULL)
-	{
-	
-		int pipes[2];
-		pipe(pipes);
-		pid_t child = fork();
-		
-		if (child == 0)
-		{
-			close(pipes[0]);
-			dup2(pipes[1], STDOUT_FILENO);
-			
-			/*
-			if method is "GET":
-       Use query as the value of the QUERY_STRING environment variable.
-      */
-			if (!strcmp(method, "GET"))
-			{
-				if (query)
-				{
-				
-					char queryString[1024]; 
-					strcpy(queryString, "QUERY_STRING=");
-					strcat(queryString, query);
-					char *env[] = { queryString, NULL };
-					execle(uri, uri, NULL, env);
-				}
-				else
-				{
-					execl(uri, uri, NULL);
-				}
-			}
-			
-			/*
-			if method is "POST":
-		    Use boundary to split the body of the request (see the project
-		    page for an example). The size parameter is only used if the POST
-		    request is for uploading a file, and is the size of the data to
-		    be uploaded.
-      */
-			else if (!strcmp(method, "POST"))
-			{
-			
-				char* db = NULL;
-				char* hash = NULL;
-				char* record = NULL;
-				char* string = NULL;
-				char* current = NULL;
-				
-				string = strstr(body, boundary);
-				string += strlen(boundary);
-				string = strstr(string, "name=\"");
-				
-				intptr_t  start;
-				intptr_t  end;
-				
-				char* token = NULL;
-				char* type = NULL;
-				
-				while (string)
-				{
-					token = strdup(string+6);
-					start = (intptr_t) string + 6;
-					string = strstr(string, "\"\r\n");
-					end = (intptr_t)  string;
-					
-					type = strndup(token, (size_t) end-start);
-					//printf("Current: %s\n", current);
-					
-					string = strstr(string, "\r\n\r\n");
-					token = strdup(string+4);
-					start = (intptr_t) string + 4;
-					string += 4;
-					string = strstr(string, "\r\n");
-					end = (intptr_t) string;
-					
-					current = strndup(token, (size_t) end-start);
-					//printf("Current: %s\n", current);
-					
-					if(!strcmp(type, "db"))
-					{
-						db = malloc(3+strlen(current)+1);
-						snprintf(db, 3+strlen(current)+1, "db=%s", current);
-					}
-					else if(!strcmp(type, "hash"))
-					{
-						hash = malloc(5+strlen(current)+1);
-						snprintf(db, 5+strlen(current)+1, "hash=%s", current);
-					}
-					else
-					{
-						record = malloc(7+strlen(current)+1);
-						snprintf(record, 7+strlen(current)+1, "record=%s", current);
-					}
-					
-					string = strstr(string, "name=\"");
-				}
-				
-				char *env[] = { db, record, hash };
-				execle(uri, uri, NULL, env);
-			}
-			//execl(uri, uri, NULL);
-			exit(1);
-			
-		}
-		
-		close(pipes[1]);
-		
-		char buffer[BUFFER_LENGTH];
-  		memset (&buffer, 0, BUFFER_LENGTH);
-  		
-		ssize_t bytes = read(pipes[0], buffer, BUFFER_LENGTH);
-		
-		if (bytes < 0)
-		{
-			return NULL;
-		}
-		ssize_t length;
-		
-		if (!strcmp(version, "HTTP/1.0"))
-		{
-			length = snprintf(NULL, 0, "%s 200 OK\r\n"
-			"Content-Type: text/html; charset=UTF-8\r\n"
-			"Content-Length: %ld\r\n\r\n", version, bytes);
-				
-			response = malloc(length + 1);
-				
-			snprintf(response, length+1, "%s 200 OK\r\n"
-			"Content-Type: text/html; charset=UTF-8\r\n"
-			"Content-Length: %ld\r\n\r\n", version, bytes);
-		}
-		else if (!strcmp(version, "HTTP/1.1"))
-		{
-			length = snprintf(NULL, 0, "%s 200 OK\r\n"
-			"Content-Type: text/html; charset=UTF-8\r\n"
-			"Content-Length: %ld\r\n"
-			"Connection: close\r\n\r\n", version, bytes);
-				
-			response = malloc(length + 1);
-				
-			snprintf(response, length+1, "%s 200 OK\r\n"
-			"Content-Type: text/html; charset=UTF-8\r\n"
-			"Content-Length: %ld\r\n"
-			"Connection: close\r\n\r\n", version, bytes);
-		}
-		char contents[BUFFER_LENGTH];
-		strcpy(contents, buffer);
-		
-		response = realloc(response, length + bytes + 1);
-		strncat(response, contents, length + bytes + 1);
-		
-	}
-	else
-	{
-		response = strdup ("HTTP/1.0 404 Not Found" CRLF CRLF);
-	}
+    {
+
+      int pipes[2];
+      pipe (pipes);
+      pid_t child = fork ();
+
+      if (child == 0)
+        {
+          close (pipes[0]);
+          dup2 (pipes[1], STDOUT_FILENO);
+
+          /*
+          if method is "GET":
+          Use query as the value of the
+          QUERY_STRING environment variable.
+          */
+          if (!strcmp (method, "GET"))
+            {
+              if (query)
+                {
+
+                  char queryString[1024];
+                  strcpy (queryString, "QUERY_STRING=");
+                  strcat (queryString, query);
+                  char *env[] = { queryString, NULL };
+                  execle (uri, uri, NULL, env);
+                }
+              else
+                {
+                  execl (uri, uri, NULL);
+                }
+            }
+
+          /*
+          if method is "POST":
+          Use boundary to split the body
+          of the request (see the project page for an example). The size
+          parameter is only used if the POST request is for uploading a file,
+          and is the size of the data to be uploaded.
+          */
+          else if (!strcmp (method, "POST"))
+            {
+              char *db = NULL;
+              char *hash = NULL;
+              char *record = NULL;
+              char *string = NULL;
+
+              string = strstr (body, boundary);
+              string += strlen (boundary);
+              string = strstr (string, "name=\"");
+              int start;
+              int end;
+              char token[1024];
+              char type[1024];
+              char current[1024];
+              while (string)
+                {
+                  strcpy (token, string + 6);
+                  start = (intptr_t)string + 6;
+                  string = strstr (string, "\"\r\n");
+                  end = (intptr_t)string;
+                  strncpy (type, token, (size_t)end - start);
+                  type[end - start] = '\0';
+
+                  string = strstr (string, "\r\n\r\n");
+                  strcpy (token, string + 4);
+
+                  string += 4;
+                  start = (intptr_t)string;
+
+                  string = strstr (string, "\r\n");
+                  end = (intptr_t)string;
+
+                  strncpy (current, token, (size_t)end - start);
+
+                  current[end - start] = '\0';
+
+                  if (!strcmp (type, "db"))
+                    {
+                      db = malloc (strlen ("db=") + strlen (current) + 1);
+                      snprintf (db, strlen ("db=") + strlen (current) + 1,
+                                "db=%s", current);
+                    }
+                  else if (!strcmp (type, "hash"))
+                    {
+                      hash = malloc (strlen ("hash=") + strlen (current) + 1);
+                      snprintf (hash, strlen ("hash=") + strlen (current) + 1,
+                                "hash=%s", current);
+                    }
+                  else if (!strcmp (type, "record"))
+                    {
+                      record
+                          = malloc (strlen ("record=") + strlen (current) + 1);
+                      snprintf (record,
+                                strlen ("record=") + strlen (current) + 1,
+                                "record=%s", current);
+                      printf ("Added record\n");
+                      printf ("%s\n", record);
+                    }
+
+                  string = strstr (string, "name=\"");
+                }
+
+              char *env[] = { db, record, hash };
+              execle (uri, uri, NULL, env);
+            }
+          // execl(uri, uri, NULL);
+          exit (1);
+        }
+
+      close (pipes[1]);
+
+      char buffer[BUFFER_LENGTH];
+      memset (&buffer, 0, BUFFER_LENGTH);
+
+      ssize_t bytes = read (pipes[0], buffer, BUFFER_LENGTH);
+
+      if (bytes < 0)
+        {
+          return NULL;
+        }
+      ssize_t length;
+
+      if (!strcmp (version, "HTTP/1.0"))
+        {
+          length = snprintf (NULL, 0,
+                             "%s 200 OK\r\n"
+                             "Content-Type: text/html; charset=UTF-8\r\n"
+                             "Content-Length: %ld\r\n\r\n",
+                             version, bytes);
+
+          response = malloc (length + 1);
+
+          snprintf (response, length + 1,
+                    "%s 200 OK\r\n"
+                    "Content-Type: text/html; charset=UTF-8\r\n"
+                    "Content-Length: %ld\r\n\r\n",
+                    version, bytes);
+        }
+      else if (!strcmp (version, "HTTP/1.1"))
+        {
+          length = snprintf (NULL, 0,
+                             "%s 200 OK\r\n"
+                             "Content-Type: text/html; charset=UTF-8\r\n"
+                             "Content-Length: %ld\r\n"
+                             "Connection: close\r\n\r\n",
+                             version, bytes);
+
+          response = malloc (length + 1);
+
+          snprintf (response, length + 1,
+                    "%s 200 OK\r\n"
+                    "Content-Type: text/html; charset=UTF-8\r\n"
+                    "Content-Length: %ld\r\n"
+                    "Connection: close\r\n\r\n",
+                    version, bytes);
+        }
+      char contents[BUFFER_LENGTH];
+      strcpy (contents, buffer);
+
+      response = realloc (response, length + bytes + 1);
+      strncat (response, contents, length + bytes + 1);
+    }
+  else
+    {
+      response = strdup ("HTTP/1.0 404 Not Found" CRLF CRLF);
+    }
   return response;
 
   // TODO [FULL]: Set the environment variables needed for the CGI programs
@@ -218,5 +230,4 @@ cgi_response (char *uri, char *version, char *method, char *query,
   // variable to be the query parameter. For POST requests, you will need
   // to look through the body of the HTTP request, splitting based on the
   // boundary values (see the project description for an example).
-  
 }
